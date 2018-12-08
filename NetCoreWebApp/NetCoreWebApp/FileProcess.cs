26daeb066255aba88b4798fdcbe5543de6c2373d
DavidWants2Learn <David.Oh.331@my.csun.edu>
2018-12-08 15:50:31 -0800

    Inserting Test Class for testing file paths

---
+++ NetCoreWebApp/NetCoreWebApp/FileProcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace NetCoreWebApp
+{
+    public class FileProcess
+    {
+        public bool File_exists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+
+            return File.Exists(fileName);
+        }
+    }
+}
---
+++ NetCoreWebApp/TestClass/FileProcessTest.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestClass
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+
+            // Arrange
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            // Act
+            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+        
+            // Assert
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
